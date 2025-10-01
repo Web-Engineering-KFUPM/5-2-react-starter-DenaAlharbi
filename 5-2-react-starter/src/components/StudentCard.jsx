028a2e300c04eca979b6a78d285a527a0c1a159c
DenaAlharbi <s202250560@kfupm.edu.sa>
2025-10-01 11:04:29 +0300

    Task1 until the third

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,12 @@
+function StudentCard() {
+    return (
+        <div>
+            <h3>Name: Dena Alharbi</h3>
+            <p>ID: 202250560</p>
+            <p>Department: Software Engineering</p>
+
+        </div>
+    );
+}
+
+export default StudentCard;
